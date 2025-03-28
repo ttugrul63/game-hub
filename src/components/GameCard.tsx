9f5c1974dc647c079fd311cb4559c42e8afe2e72
ttugrul63 <tugaytugrul@live.com>
2025-03-28 07:07:56 +0300

    game card touch ups

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,13 +12,13 @@ const GameCard = ({ game }: GameCardProps) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack marginBottom={3} justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic}></CriticScore>
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
